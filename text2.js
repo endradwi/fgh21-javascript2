d6310735202988a4b92ebdadd0ef7f703122bee6
endradwi <94545255+endradwi@users.noreply.github.com>
2024-06-06 15:50:02 +0700

    Add files via upload

--- text2.js
+++ text2.js
@@ -1,13 +1,65 @@
-const names = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabets', 'Ella', 'Faith', 'Olivia', 'Penelope'];
+const names = [
+    'Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 
+    'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 
+    'Diana', 'Elizabets', 'Ella', 'Faith', 'Olivia', 'Penelope'];
 
-function filterKata(filter, limit, panggil) {
-    const kata = names.filter(function(name) {
-        return name.toLowerCase().includes(filter.toLowerCase())
-    })
-    const jumlah = kata.slice(0, limit)
-    panggil(jumlah)
+const filterKata = function (filter, limit, callback) {
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowercase = "abcdefghijklmnopqrstuvwxyz"
+    let searchResult = []
+
+for(let i = 0; i < names.length; i++){
+    const personName = names[i]
+    let result = ""
+
+    for(let i = 0; i < personName.length; i++){
+        let converted = false
+        for(let j = 0; j < uppercase.length; j++){
+            if(converted === false){
+                if(personName[i] === uppercase[j]){
+                    result = result + lowercase[j]
+                    converted = true
+                }
+                if(j === 25){
+                    if(personName[i] !== uppercase[j]){
+                        result = result + personName[i]
+                        converted = true
+                    }
+                }
+            }
+        }
+    }
+
+let isFound = false
+    for(let m = 0; m < result.length; m++){
+        let segmen = ""
+    if(result[m+1] !== undefined){
+        segmen=result[m] + result[m+1]
+    }else{
+        segmen = result[i]
+    }
+
+    if(segmen === filter){
+        isFound = true
+    }
 }
-function output(gabung) {
-    console.log(gabung.join(', '))
+
+if(isFound === true){
+    searchResult = [...searchResult, personName]
 }
-filterKata("an", 3, output)
+}
+callback(searchResult, limit)
+}
+
+function limitResult  (arr, lim){
+    let result = []
+    for(let a=0; a<lim; a++){
+        if(arr[a]){
+            result = [...result, arr[a]]
+        }
+    }
+    console.log(result)
+}
+
+
+filterKata("an", 3, limitResult)
